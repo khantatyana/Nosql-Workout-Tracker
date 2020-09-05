d6c2aecceb0fe0f74f8c22698ff0f46a59d3754f
Tatyana <hanyatan@gmail.com>
2020-09-05 11:56:30 -0400

    fixed duration

--- models/workout.js
+++ models/workout.js
@@ -27,8 +27,8 @@ const workoutSchema = new Schema ({
 
 workoutSchema.virtual("totalDuration").get(function() {
     return this.exercises.reduce((total, exercise) => {
-        total += exercise;
-    });
+        return total += exercise.duration;
+    }, 0);
 });
 const Workout = mongoose.model("Workout", workoutSchema);
 module.exports = Workout;
